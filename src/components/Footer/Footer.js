4376f2089c4d96997d6a2756d2479ebdb29b728d
Dana <sundetbayeva.dana@yandex.ru>
2022-01-23 23:40:06 +0500

    добавлена стартовая страница 768

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,15 +6,17 @@ import { Link } from 'react-router-dom';
 function Footer () {
   return (
     <div className="footer">
-      <h2 className="footer__heading">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
-      <div className="footer__container">
-        <p className="footer__copyright">
-          &#169;2022
-        </p>
-        <div className="footer__links">
-          <Link to="#" className="footer__link">Яндекс.Практикум</Link>
-          <Link to="#" className="footer__link">Github</Link>
-          <Link to="#" className="footer__link">Facebook</Link>
+      <div className="footer__content">
+        <h2 className="footer__heading">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
+        <div className="footer__container">
+          <p className="footer__copyright">
+            &#169;2022
+          </p>
+          <div className="footer__links">
+            <Link to="#" className="footer__link">Яндекс.Практикум</Link>
+            <Link to="#" className="footer__link">Github</Link>
+            <Link to="#" className="footer__link">Facebook</Link>
+          </div>
         </div>
       </div>      
     </div>
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -4,23 +4,25 @@ import { Link } from 'react-router-dom';
 
 function Portfolio () {
   return (
-    <div className="portfolio">      
-      <p className="portfolio__text">Портфолио</p>
-      <div className="portfolio__link-container">
-        <p className="portfolio__link-text">Статичный сайт</p>
-        <Link to="#" className="portfolio__link">
-        </Link>    
-      </div>
-      <div className="portfolio__link-container">
-        <p className="portfolio__link-text">Адаптивный сайт</p>
-        <Link to="#" className="portfolio__link">
-        </Link>
-      </div>
-      <div className="portfolio__link-container">
-        <p className="portfolio__link-text">Одностраничное приложение</p>
-        <Link to="#" className="portfolio__link">
-        </Link> 
-      </div>
+    <div className="portfolio">
+      <div className="portfolio__content">
+        <p className="portfolio__text">Портфолио</p>
+        <div className="portfolio__link-container">
+          <p className="portfolio__link-text">Статичный сайт</p>
+          <Link to="#" className="portfolio__link">
+          </Link>    
+        </div>
+        <div className="portfolio__link-container">
+          <p className="portfolio__link-text">Адаптивный сайт</p>
+          <Link to="#" className="portfolio__link">
+          </Link>
+        </div>
+        <div className="portfolio__link-container">
+          <p className="portfolio__link-text">Одностраничное приложение</p>
+          <Link to="#" className="portfolio__link">
+          </Link> 
+        </div>
+      </div>      
     </div>
   )
 }
