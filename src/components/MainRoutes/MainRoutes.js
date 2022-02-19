b0d2b67e1e2cb2ba4badc381ff325123aa59f6ff
Dana <sundetbayeva.dana@yandex.ru>
2022-02-20 00:00:58 +0500

    fixbugs

--- src/components/MainRoutes/MainRoutes.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Navigate, Outlet } from "react-router-dom";
-
-function MainRoutes() {
-  return (JSON.parse(localStorage.getItem('loggedIn')) === true) ? <Outlet /> : <Navigate to="/" replace/>
-};
-
-export default MainRoutes;
-
